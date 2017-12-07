7ae1df040e68b882360d8fc8fb6ee1532eefdbb1
Patrick <pat.rodriguez108@gmail.com>
2017-12-07 15:17:55 -0600

    Add psuedo

--- concat_words.rb
+++ concat_words.rb
@@ -1,6 +1,15 @@
+# find all possible concats of words
+# check s for inclusion of concats
+# find starting indices of concats
+
 def find_substring(s, words)
-    
+    a_join = words.join
+    if s.include?(a_join)
+    	
+    end
 end
 
 s = "barfoothefoobarman"
 words = ["foo", "bar"]
+
+find_substring(s, words)
